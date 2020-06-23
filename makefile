2848e40e63f1ecb37f6b93972d2d8a6cd77b8710
tdallas <tdallas@itba.edu.ar>
2020-06-23 12:55:42 -0300

    fix: lsb4 embedding and extracting, both are working. Integration with parser

--- makefile
+++ makefile
@@ -6,14 +6,14 @@ CFLAGS = -Wall
 # testsParser: parser.c ./tests/testParser.c ./tests/testSuite.c ./tests/testRC4.c
 # 	$(CC) -o parser parser.c ./tests/testParser.c ./tests/testSuite.c ./tests/testRC4.c $(CFLAGS)
 
-# testsLSB: ./tests/testLsb.c  ./stego/lsbEncrypt.c ./tests/testLsb.c ./stego/lsbDecrypt.c
-# 	$(CC) -o testLSB ./tests/testLsb.c ./stego/lsbEncrypt.c ./stego/lsbDecrypt.c bmpParser.c $(CFLAGS)
+# testsLSB: ./tests/testLsb.c  ./stego/lsbEmbed.c ./tests/testLsb.c ./stego/lsbExtract.c
+# 	$(CC) -o testLSB ./tests/testLsb.c ./stego/lsbEmbed.c ./stego/lsbExtract.c bmpParser.c $(CFLAGS)
 
 main: 
-	$(CC) -o main main.c ./stego/lsbEncrypt.c ./stego/lsbDecrypt.c parser.c bmpParser.c extract.c embed.c $(CFLAGS)
+	$(CC) -o main main.c ./stego/lsbEmbed.c ./stego/lsbExtract.c parser.c bmpParser.c extract.c embed.c $(CFLAGS)
 
 # bmpParserTest: 
-# 	$(CC) -o main ./tests/bmpParser.c ./stego/lsbEncrypt.c ./stego/lsbDecrypt.c parser.c bmpParser.c extract.c embed.c $(CFLAGS)
+# 	$(CC) -o main ./tests/bmpParser.c ./stego/lsbEmbed.c ./stego/lsbExtract.c parser.c bmpParser.c extract.c embed.c $(CFLAGS)
 
 
 clean: 
--- stego/lsbExtract.c
+++ stego/lsbExtract.c
@@ -5,10 +5,10 @@
 #include <stdlib.h>
 #include <string.h>
 
-// uint8_t replaceNthLSB(const uint8_t bmpByte, const uint8_t stegoByte, unsigned int cbCursor, unsigned int bitToReplace)
+/**
+ * Keep in mind these functions could be abstracted a lot
+ */
 
-// We should change this function's name to lsb1Extraction or something like that
-// Encrypt and decrypt seems unreasonable cause stego could be a non cipher one
 uint8_t *lsb1Extract(const uint8_t *bmpFile, const size_t stegoSize, const size_t bmpSize, const size_t widthInBytes)
 {
     int bmpIndex = bmpSize - 1;
@@ -40,5 +40,54 @@ uint8_t *lsb1Extract(const uint8_t *bmpFile, const size_t stegoSize, const size_
 
     stego[stegoSize] = 0;
 
+    return stego;
+}
+
+uint8_t *lsb4Extract(const uint8_t *bmpFile, const size_t stegoSize, const size_t bmpSize, const size_t widthInBytes)
+{
+    printf("ENTRO\n");
+    int bmpIndex = bmpSize - 1;
+    int stegoIndex = 0;
+    int stegoBitCursor = 7;
+    uint8_t *stego = malloc(stegoSize + 1);
+    int rowCursor = widthInBytes - 1;
+    int stegoCount = stegoSize - 1;
+    while (bmpIndex >= 0 && stegoCount >= 0)
+    {
+        uint8_t auxByte;
+        while (stegoBitCursor >= 0 && bmpIndex >= 0 && stegoCount >= 0)
+        {
+            printf("%d\n", bmpIndex - rowCursor);
+            // FIRST LSB
+            uint8_t firstBitToReplace = getCurrentBitOf(bmpFile[bmpIndex - rowCursor], 0);
+            auxByte = replaceNthLSB(auxByte, firstBitToReplace, 0, stegoBitCursor--);
+            // SECOND LSB
+            uint8_t secondBitToReplace = getCurrentBitOf(bmpFile[bmpIndex - rowCursor], 1);
+            auxByte = replaceNthLSB(auxByte, secondBitToReplace, 0, stegoBitCursor--);
+            // THIRD LSB
+            uint8_t thirdBitToReplace = getCurrentBitOf(bmpFile[bmpIndex - rowCursor], 2);
+            auxByte = replaceNthLSB(auxByte, thirdBitToReplace, 0, stegoBitCursor--);
+            // FOURTH LSB
+            uint8_t fourthBitToReplace = getCurrentBitOf(bmpFile[bmpIndex - rowCursor], 3);
+            auxByte = replaceNthLSB(auxByte, fourthBitToReplace, 0, stegoBitCursor--);
+
+            // printingBits(auxByte);
+
+            rowCursor--;
+            if (rowCursor == -1)
+            {
+                bmpIndex -= widthInBytes;
+                rowCursor = widthInBytes - 1;
+            }
+        }
+        stego[stegoIndex] = auxByte;
+
+        stegoIndex++;
+        stegoBitCursor = 7;
+        stegoCount--;
+    }
+
+    stego[stegoSize] = 0;
+    printf("RETORNA\n");
     return stego;
 }
